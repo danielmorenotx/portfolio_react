baf6c3527c59cd44e79091be5557333b5083fe3f
danielmoreno <dmoreno@indeed.com>
2024-06-12 09:04:59 -0500

    updated links

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
-        <section id="home-section">
-            <div className="home-info">
+        <section id="about-section">
+            <div className="about-info">
                 <h2 className="subheaders">About Me</h2>
                 <p>
                     Daniel is a software developer in training at Indeed. Before starting Indeed's apprenticeship porgram, he worked for almost 9 years on the Trust & Safety team, during which he held multiple roles and specialized in querying data and training other members of the Trust & Safety team to write efficient queries as well.
