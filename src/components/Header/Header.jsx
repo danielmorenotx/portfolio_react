30edd3839a4f8c98c2492baa502c61b6e7e45479
danielmoreno <dmoreno@indeed.com>
2024-06-12 13:02:15 -0500

    added mobile friendly menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,38 @@
 import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import List from "../List/List"
 import "./Header.css"
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false); // initial state of menuOpen is 'false'
+    
+    const handleOpenMenu = () => {
+        setMenuOpen(!menuOpen); // sets state to the opposite of what it is when clicked
+    }
+
+
     return (
-        <section className="heading-nav">
-            <h1><img src="./images/Daniel_logo.png" alt="Daniel Moreno" title="Daniel Moreno"/></h1>
-            <nav>
-                <ul>
-                    <li className="header-link"><a href="#about-section">About</a></li>
-                    <li className="header-link"><a href="#gallery-section">My Work</a></li>
-                    <li className="header-link"><a href="#followme-section">Follow Me</a></li>
-                    <li className="header-link"><a href="#contact-section">Contact</a></li>
-                </ul>
-            </nav>
-        </section>
+        <header className="heading-nav">
+            <div className="heading-content">
+                <h1><img src="./images/Daniel_logo.png" alt="Daniel Moreno" title="Daniel Moreno"/></h1>
+                <nav className={`full-nav ${menuOpen ? "open" : ""}`}>
+                    <ul>
+                        <li className="header-link"><a href="#about-section">About</a></li>
+                        <li className="header-link"><a href="#gallery-section">My Work</a></li>
+                        <li className="header-link"><a href="#followme-section">Follow Me</a></li>
+                        <li className="header-link"><a href="#contact-section">Contact</a></li>
+                    </ul>
+                    
+                    <button className="nav-button nav-close-button" onClick={handleOpenMenu}>
+                        <FaTimes />
+                    </button>
+                </nav>
+            </div>
+
+            <button className="nav-button" onClick={handleOpenMenu}>
+                <FaBars />
+            </button>
+        </header>
     );
 };
 
