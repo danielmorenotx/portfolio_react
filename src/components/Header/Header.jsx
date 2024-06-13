01d937df579d5eaa6b3388f8c0d2c80b9af8d516
danielmoreno <dmoreno@indeed.com>
2024-06-13 12:11:02 -0500

    updated so that it is offset and not covered by the header when it scrolls to my work

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,6 +9,21 @@ const Header = () => {
         setMenuOpen(!menuOpen); // sets state to the opposite of what it is when clicked
     }
 
+    const handleNavClick = (event, sectionId) => {
+        event.preventDefault(); // prevents default anchor behavior
+        const offset = 250; // sets value of my offset for then scroll
+
+        const section = document.querySelector(sectionId);
+        const sectionTop = section.getBoundingClientRect().top + window.scrollY - offset;
+
+        window.scrollTo({
+            top: sectionTop,
+            behavior: "smooth"
+        });
+
+        event.currentTarget.blur();
+    }
+
 
     return (
         <header className="heading-nav">
@@ -18,10 +33,18 @@ const Header = () => {
                 
                 <nav className={`full-nav ${menuOpen ? "open" : ""}`}>
                     <ul className="nav-list">
-                        <li className="header-link"><a href="#" className="header-link">About</a></li>
-                        <li className="header-link"><a href="#gallery-section" className="header-link">My Work</a></li>
-                        <li className="header-link"><a href="#followme-section" className="header-link">Follow Me</a></li>
-                        <li className="header-link"><a href="#contact-section" className="header-link">Contact</a></li>
+                        <li className="header-link">
+                            <a href="#" className="header-link">About</a>
+                        </li>
+                        <li className="header-link">
+                            <a href="#gallery-section" className="header-link" onClick={(e) => handleNavClick(e, "#gallery-section")}>My Work</a>
+                        </li>
+                        <li className="header-link">
+                            <a href="#followme-section" className="header-link">Follow Me</a>
+                        </li>
+                        <li className="header-link">
+                            <a href="#contact-section" className="header-link">Contact</a>
+                        </li>
                     </ul>
                     
                     <button className="nav-button nav-close-button" onClick={handleOpenMenu}>
