dda931670c017f9f1cba2ed193918f22ebd751e8
danielmoreno <dmoreno@indeed.com>
2024-06-13 11:21:09 -0500

    updated header to be completely fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,13 +13,15 @@ const Header = () => {
     return (
         <header className="heading-nav">
             <div className="heading-content">
+                
                 <h1><img src="./images/Daniel_logo.png" alt="Daniel Moreno" title="Daniel Moreno"/></h1>
+                
                 <nav className={`full-nav ${menuOpen ? "open" : ""}`}>
-                    <ul>
-                        <li className="header-link"><a href="#about-section">About</a></li>
-                        <li className="header-link"><a href="#gallery-section">My Work</a></li>
-                        <li className="header-link"><a href="#followme-section">Follow Me</a></li>
-                        <li className="header-link"><a href="#contact-section">Contact</a></li>
+                    <ul className="nav-list">
+                        <li className="header-link"><a href="#about-section" className="header-link">About</a></li>
+                        <li className="header-link"><a href="#gallery-section" className="header-link">My Work</a></li>
+                        <li className="header-link"><a href="#followme-section" className="header-link">Follow Me</a></li>
+                        <li className="header-link"><a href="#contact-section" className="header-link">Contact</a></li>
                     </ul>
                     
                     <button className="nav-button nav-close-button" onClick={handleOpenMenu}>
--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
                     Daniel is a software developer in training at Indeed. Before starting Indeed's apprenticeship porgram, he worked for almost 9 years on the Trust & Safety team, during which he held multiple roles and specialized in querying data and training other members of the Trust & Safety team to write efficient queries as well.
                 </p>
             </div>
-            <img src="images/oregon_eclipse.jpg" alt="Oregon eclipse" id="oregon-eclipse"></img>
+            <img src="images/picture_of_me.jpg" alt="Oregon eclipse" id="oregon-eclipse"></img>
         </section>
     )
 }
