20e20b0f41ab9921598c3a457763e6272873e4f7
danielmoreno <dmoreno@indeed.com>
2024-06-13 17:16:51 -0500

    updated about

--- src/components/Main/About/About.jsx
+++ src/components/Main/About/About.jsx
@@ -6,11 +6,19 @@ const About = () => {
             <div className="about-info">
                 <h2 className="subheaders">About Me</h2>
                 <p>
-                    Daniel is a software developer in training at Indeed. Before starting Indeed's apprenticeship porgram, he worked for almost 9 years on the Trust & Safety team, during which he held multiple roles and specialized in querying data and training other members of the Trust & Safety team to write efficient queries as well. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam sequi ipsam ratione neque dolore modi at fugit non, culpa incidunt deleniti quasi quos eos, molestiae expedita explicabo minus libero? In.
-                    Commodi, ad corrupti ullam officia quis earum culpa repellendus eum eius. Ratione aut vitae similique accusantium blanditiis nulla eius, repudiandae neque quisquam ut quo harum commodi nesciunt sequi eum. Non.
-                    Corrupti sequi dignissimos commodi possimus? Corrupti architecto harum facere adipisci officia, hic amet expedita, similique magnam officiis ad quasi eius modi consequuntur fuga perspiciatis dolorum. Mollitia minus neque laudantium placeat?
-                    Odio beatae porro error modi sapiente excepturi expedita quod id nulla culpa ullam facilis dicta aperiam doloribus provident, aliquid placeat qui quae accusantium. Optio, error sit nam est placeat iste.
-                    Est laborum harum, repudiandae dolorem numquam voluptatem laudantium veritatis voluptates tempore asperiores quas autem hic rerum non voluptas possimus nesciunt perferendis rem sapiente nihil, facilis libero. Iste, culpa fugit? Temporibus?
+                    Daniel is a software developer apprentice at Indeed, currently participating in Indeed's pilot Boost Apprenticeship Program. Prior to this, he dedicated nearly nine years to the Trust & Safety team at Indeed, where he excelled in various roles.
+                </p>
+                <p>
+                    In his final position within the Trust & Safety team, Daniel developed a keen interest in data extraction and visualization. Driven by this newfound passion, he self-taught SQL and Tableau, earning the Tableau Desktop Specialist Certificate. Leveraging these skills, he identified data redundancies and streamlined processes within his team.
+                </p>
+                <p>
+                    Eager to further enhance his technical capabilities, Daniel enrolled in the Google Data Analytics Professional course offered by Coursera, a rigorous six-month program. Upon completion, his enthusiasm for data analytics deepened, prompting him to learn Pandas to bolster his qualifications as a data analyst.
+                </p>
+                <p>
+                    It was during this period that Indeed launched its pilot apprenticeship program. After successfully applying, Daniel decided to pivot towards software development, a field that seamlessly integrates his passion for data analytics. Recently, he completed General Assembly's Software Engineering bootcamp, gaining proficiency in Java and React.
+                </p>
+                <p>
+                    Daniel is excited to continue applying his diverse skill set at Indeed and is enthusiastic about pursuing side projects that he is now empowered to create.
                 </p>
             </div>
             <img src="images/picture_of_me.jpg" alt="Oregon eclipse" id="oregon-eclipse"></img>
