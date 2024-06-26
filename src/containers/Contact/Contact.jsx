c90ca3a2a4c54e2ecf57e8d0f01ba5be5fbcd8ce
danielmoreno <dmoreno@indeed.com>
2024-06-26 16:40:44 -0500

    updated contact form to work

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,18 +1,31 @@
 import "./Contact.css"
+import emailjs from 'emailjs-com';
+import {emailjs_service_id, emailjs_template_id, emailjs_user_id} from "../../private/private";
 
 const Contact = () => {
+
+    function sendEmail(e) {
+        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+    
+        emailjs.sendForm(emailjs_service_id, emailjs_template_id, e.target, emailjs_user_id)
+          .then((result) => {
+              window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
 
     return (
         <section id="contact-section">
             <h2 className="subheaders">Want to get in touch?</h2>
             <p>Fill out this form to send me an email!</p>
 
-            <form action="/submit-form">
+            <form action="/submit-form" onSubmit={sendEmail}>
                 <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" required></input>
+                <input type="text" id="name" name="from_name" required></input>
         
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required></input>
+                <input type="email" id="email" name="from_email" required></input>
         
                 <label htmlFor="message">Message:</label>
                 <textarea id="message" name="message" rows="4" required></textarea>
