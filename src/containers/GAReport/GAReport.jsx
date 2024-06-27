1037f1d97bcfc7045572278cd8eb3a881c9fc78e
danielmoreno <dmoreno@indeed.com>
2024-06-27 16:10:33 -0500

    updated reports to make more DRY

--- src/containers/GAReport/GAReport.jsx
+++ src/containers/GAReport/GAReport.jsx
@@ -22,6 +22,89 @@ const CustomNextArrow = (props) => {
 };
 
 const GAReport = () => {
+
+    const mainReportData = [
+        {
+          week: 1,
+          topic: "Dev setup, git & HTML",
+          feedback: "Asks questions and adds to questions/discussions in voice & text chat. Friendly attitude while doing so. Consistent with homework thus far."
+        },
+        {
+          week: 2,
+          topic: "JS (Looping, Arrays & Functions)",
+          feedback: "Up to date on deliverables and excellent work on them. Full attendance, mostly consistent with exit tickets. Consistently communicating in chat."
+        },
+        {
+          week: 3,
+          topic: "JS (Dom, Event Listeners, API calls, Fetching APIs)",
+          feedback: "Up to date on deliverables. Based on labs and questions, student is progressing very well."
+        },
+        {
+          week: 4,
+          topic: "Unit Project Week",
+          feedback: "Great project. Definitely an \"I prefer backend to frontend\" attitude limiting styling efforts, but improving JS execution and experimentation. Capable of self-sufficient success or seeking help elsewhere. Trying new things outside of scope (ex: timeout)."
+        },
+        {
+          week: 5,
+          topic: "Intro to Java",
+          feedback: "Largely quiet, but competent. Occasionally asks a clarifying question, mostly in Slack."
+        },
+        {
+          week: 6,
+          topic: "Java Deep Dive",
+          feedback: "Homework is clean and well put-together. Project progress is already very high."
+        },
+        {
+          week: 7,
+          topic: "Unit Project Week",
+          feedback: "Daniel's project was excellent. Three points stand out: code comprehension, validation, and breaking things into sub-problems. He could clearly explain both his own code and what functions from outside packages could do, and spent the latter half of his project time working on creating a robust system for ensuring clean input to his system."
+        },
+        {
+          week: 8,
+          topic: "SQL",
+          feedback: "Seems to have the hang of SQL. Not enough variation in SQL code or personal interaction this week to make any observations."
+        },
+        {
+          week: 9,
+          topic: "Springboot",
+          feedback: "Still producing good and consistent code. Still reaches out if he encounters a problem, which is very infrequently."
+        },
+        {
+          week: 10,
+          topic: "Springboot",
+          feedback: "n/a"
+        },
+        {
+          week: 11,
+          topic: "Springboot",
+          feedback: "Like many other students, this week was much more difficult for Daniel. He's been wanting the class recordings and been pleased to have more time to work on the assignments, which hasn't always been the case. His response to something being more difficult is to ask for resources, work hard on it, and produce good code at the end."
+        },
+        {
+          week: 12,
+          topic: "Springboot Project Week",
+          feedback: "Has reached out for much more help with this project, both to instructors and to fellow students. His work with Kathy has paid off, as he has implemented one of the harder concepts into his codebase. Project is looking very good thus far."
+        },
+        {
+          week: 13,
+          topic: "React",
+          feedback: "n/a"
+        },
+        {
+          week: 14,
+          topic: "React",
+          feedback: "Daniel's gotten more quiet as the course progressed, but always has asked questions if he had a blocker. He tackles assignments with a drive for efficiency, both in the code and the completion of the assignment. He's been a very good student for this instructional portion of the course and we've been happy to have him."
+        },
+        {
+          week: 15,
+          topic: "React/Capstone",
+          feedback: "Learning a lot of concepts in executing on the project. Project is on track to be completed on time."
+        },
+        {
+          week: 16,
+          topic: "Capstone",
+          feedback: "n/a"
+        }
+      ]
     
     const settings = {
         dots: true,
@@ -46,86 +129,17 @@ const GAReport = () => {
             <section className="reports-section">
                 <h2>Weekly Reports</h2>
                 <div className="report-container">
-                    <div className="feedback-card">
-                        <h3>Week 1</h3>
-                        <p><span>Topic:</span> Dev setup, git & HTML</p>
-                        <p><span>Feedback:</span> Asks questions and adds to questions/discussions in voice & text chat. Friendly attitude while doing so. Consistent with homework thus far.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 2</h3>
-                        <p><span>Topic:</span> JS (Looping, Arrays & Functions)</p>
-                        <p><span>Feedback:</span> Up to date on deliverables and excellent work on them. Full attendance, mostly consistent with exit tickets. Consistently communicating in chat.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 3</h3>
-                        <p><span>Topic:</span> JS (Dom, Event Listeners, API calls, Fetching APIs)</p>
-                        <p><span>Feedback:</span> Up to date on deliverables. Based on labs and questions, student is progressing very well.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 4</h3>
-                        <p><span>Topic:</span> Unit Project Week</p>
-                        <p><span>Feedback:</span> Great project. Definitely an "I prefer backend to frontend" attitude limiting styling efforts, but improving JS execution and experimentation. Capable of self-sufficient success or seeking help elsewhere. Trying new things outside of scope (ex: timeout).</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 5</h3>
-                        <p><span>Topic:</span> Intro to Java</p>
-                        <p><span>Feedback:</span> Largely quiet, but competent. Occasionally asks a clarifying question, mostly in Slack.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 6</h3>
-                        <p><span>Topic:</span> Java Deep Dive</p>
-                        <p><span>Feedback:</span> Homework is clean and well put-together. Project progress is already very high.</p>
-                    </div>
-                    <div className="feedback-card">                
-                        <h3>Week 7</h3>
-                        <p><span>Topic:</span> Unit Project Week</p>
-                        <p><span>Feedback:</span> Daniel's project was excellent. Three points stand out: code comprehension, validation, and breaking things into sub-problems. He could clearly explain both his own code and what functions from outside packages could do, and spent the latter half of his project time working on creating a robust system for ensuring clean input to his system.</p>
-                    </div>
-                    <div className="feedback-card">                
-                        <h3>Week 8</h3>
-                        <p><span>Topic:</span> SQL</p>
-                        <p><span>Feedback:</span> Seems to have the hang of SQL. Not enough variation in SQL code or personal interaction this week to make any observations.</p>
-                    </div>
-                    <div className="feedback-card">                
-                        <h3>Week 9</h3>
-                        <p><span>Topic:</span> Springboot</p>
-                        <p><span>Feedback:</span> Still producing good and consistent code. Still reaches out if he encounters a problem, which is very infrequently.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 10</h3>
-                        <p><span>Topic:</span> Springboot</p>
-                        <p><span>Feedback:</span> n/a</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 11</h3>
-                        <p><span>Topic:</span> Springboot</p>
-                        <p><span>Feedback:</span> Like many other students, this week was much more difficult for Daniel. He's been wanting the class recordings and been pleased to have more time to work on the assignments, which hasn't always been the case. His response to something being more difficult is to ask for resources, work hard on it, and produce good code at the end.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 12</h3>
-                        <p><span>Topic:</span> Springboot Project Week</p>
-                        <p><span>Feedback:</span> Has reached out for much more help with this project, both to instructors and to fellow students. His work with Kathy has paid off, as he has implemented one of the harder concepts into his codebase. Project is looking very good thus far.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 13</h3>
-                        <p><span>Topic:</span> React</p>
-                        <p><span>Feedback:</span> n/a</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 14</h3>
-                        <p><span>Topic:</span> React</p>
-                        <p><span>Feedback:</span> Daniel's gotten more quiet as the course progressed, but always has asked questions if he had a blocker. He tackles assignments with a drive for efficiency, both in the code and the completion of the assignment. He's been a very good student for this instructional portion of the course and we've been happy to have him.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 15</h3>
-                        <p><span>Topic:</span> React/Capstone</p>
-                        <p><span>Feedback:</span> Learning a lot of concepts in executing on the project. Project is on track to be completed on time.</p>
-                    </div>
-                    <div className="feedback-card">
-                        <h3>Week 16</h3>
-                        <p><span>Topic:</span> Capstone</p>
-                        <p><span>Feedback:</span> n/a</p>
-                    </div>
+                {
+                    mainReportData.map((week, index) => {
+                        return (
+                            <div className="feedback-card" key={index}>
+                                <h3>{week.week}</h3>
+                                <p><span>Topic:</span> {week.topic}</p>
+                                <p><span>Feedback:</span> {week.feedback}</p>
+                            </div>
+                        )
+                    })
+                }
                 </div>
             </section>
 
