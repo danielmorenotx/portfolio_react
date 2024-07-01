d619c3426111aae3db80f2d7918b258a824d9773
danielmoreno <dmoreno@indeed.com>
2024-07-01 09:32:55 -0500

    seeing if this fixes the website

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['/Users/dmoreno/Projects/portfolio_react/src/private/private.js']
+      external: ['../../private/private.js']
     }
   }
 });
