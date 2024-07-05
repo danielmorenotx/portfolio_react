9139b05260631e1ce7d85bb864942cdd802cdba7
danielmoreno <dmoreno@indeed.com>
2024-07-05 14:46:51 -0500

    confirmation of message sent works on contact me page

--- src/containers/Contact/Contact.jsx
+++ src/containers/Contact/Contact.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import "./Contact.css"
 import emailjs from 'emailjs-com';
+import { FaCheckCircle } from "react-icons/fa";
 
 const emailjs_service_id = import.meta.env.VITE_EMAILJS_SERVICE_ID;
 const emailjs_template_id = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
@@ -7,12 +9,15 @@ const emailjs_user_id = import.meta.env.VITE_EMAILJS_USER_ID;
 
 const Contact = () => {
 
+    const [contactFormVisible, setContactFormVisible] = useState(true); // default will display the contact form
+
     function sendEmail(e) {
         e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
     
         emailjs.sendForm(emailjs_service_id, emailjs_template_id, e.target, emailjs_user_id)
           .then((result) => {
-              window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
+              // window.location.reload(); // This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
+              setContactFormVisible(false);
           }, (error) => {
               console.log(error.text);
           });
@@ -23,7 +28,7 @@ const Contact = () => {
             <h2 className="subheaders">Want to get in touch?</h2>
             <p>Fill out this form to send me an email!</p>
 
-            <form action="/submit-form" onSubmit={sendEmail}>
+            <form action="/submit-form" onSubmit={sendEmail} className={`contact-form ${contactFormVisible ? "" : "hidden"}`}>
                 <label htmlFor="name">Name:</label>
                 <input type="text" id="name" name="from_name" required></input>
         
@@ -35,6 +40,11 @@ const Contact = () => {
         
                 <input type="submit" value="Submit"></input>
             </form>
+
+            <div className={`submission-confirmation ${contactFormVisible ? "hidden" : ""}`}>
+                <FaCheckCircle className="confirmation-check" size={40}/>
+                <h3>Message sent!</h3>
+            </div>
         </section>
     )
 }
